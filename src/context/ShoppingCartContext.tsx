8faed07529fb6cfa8a6bbeed924e8a038576ce61
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-02 02:56:26 +0200

    cart context logic finished

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -18,7 +18,6 @@ type CartItem = {
   quantity: number
 }
 
-
 type ShoppingCartProviderProps = {
   children: ReactNode
 }
@@ -30,14 +29,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     return cartItems.find(item => item.id === id)?.quantity || 0
   }
 
-  function increaseCartQuantity(id: number){
+  function increaseCartQuantity(id: number) {
     setCartItems(currItems => {
       if (currItems.find(item => item.id === id) == null) {
         return [...currItems, { id, quantity: 1 }]
       } else {
         return currItems.map(item => {
           if (item.id === id) {
-            return { ...item, quantity: item.quantity + 1}
+            return { ...item, quantity: item.quantity + 1 }
           }
           else {
             return item
@@ -46,7 +45,31 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }
     })
   }
-  return <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity }}>
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems(currItems => {
+      if (currItems.find(item => item.id === id)?.quantity === 1) {
+        return currItems.filter(item => item.id !== id)
+      } else {
+        return currItems.map(item => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 }
+          }
+          else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id)
+    })
+  }
+
+  return <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>
     {children}
   </ShoppingCartContext.Provider>
 }
